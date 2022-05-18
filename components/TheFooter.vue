704ebdf661035d7d6db863fee4c9eb2a3cd71701
TeoSagli <79706196+TeoSagli@users.noreply.github.com>
2022-05-18 17:25:05 +0200

    Fixes footer

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -1,11 +1,55 @@
 <template>
-  <footer class="footer py-4">
+  <footer class="footer">
     <div class="container">
       <div class="row align-items-center">
         <div class="col-lg-4 text-lg-start">
           Copyright &copy; Your Website 2022
         </div>
-        <div class="col-lg-4 my-3 my-lg-0"></div>
+        <div class="col-lg-4 my-3 my-lg-0">
+          <!-- Partners -->
+          <!-- ========================== -->
+          <div class="container">
+            <div class="row align-items-center">
+              <div class="col-md-3 col-sm-6 my-3">
+                <a href="#!"
+                  ><img
+                    class="img-fluid img-brand d-block mx-auto"
+                    src="@/styles/assets/img/logos/microsoft.svg"
+                    alt="..."
+                    aria-label="Microsoft Logo"
+                /></a>
+              </div>
+              <div class="col-md-3 col-sm-6 my-3">
+                <a href="#!"
+                  ><img
+                    class="img-fluid img-brand d-block mx-auto"
+                    src="@/styles/assets/img/logos/google.svg"
+                    alt="..."
+                    aria-label="Google Logo"
+                /></a>
+              </div>
+              <div class="col-md-3 col-sm-6 my-3">
+                <a href="#!"
+                  ><img
+                    class="img-fluid img-brand d-block mx-auto"
+                    src="@/styles/assets/img/logos/facebook.svg"
+                    alt="..."
+                    aria-label="Facebook Logo"
+                /></a>
+              </div>
+              <div class="col-md-3 col-sm-6 my-3">
+                <a href="#!"
+                  ><img
+                    class="img-fluid img-brand d-block mx-auto"
+                    src="@/styles/assets/img/logos/ibm.svg"
+                    alt="..."
+                    aria-label="IBM Logo"
+                /></a>
+              </div>
+            </div>
+          </div>
+          <!-- ========================== -->
+        </div>
         <div class="col-lg-4 text-lg-end">
           <a class="link-dark text-decoration-none me-3" href="#!"
             >Privacy Policy</a
@@ -19,6 +63,17 @@
 
 <script>
 export default {
-  name: "TheFooter",
-};
+  name: 'TheFooter',
+}
 </script>
+
+<style scoped>
+.footer {
+  border-top: ridge;
+  text-align: center;
+  font-size: 0.9rem;
+  font-family: 'Montserrat', -apple-system, BlinkMacSystemFont, 'Segoe UI',
+    Roboto, 'Helvetica Neue', Arial, sans-serif, 'Apple Color Emoji',
+    'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji';
+}
+</style>
--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -1,5 +1,5 @@
 <template>
-  <nav class="navbar navbar-expand-lg navbar-dark sticky-top" id="mainNav">
+  <nav id="mainNav" class="navbar navbar-expand-lg navbar-dark sticky-top">
     <div class="container">
       <a class="navbar-brand" href="#page-top"
         ><img src="/assets/img/navbar-logo.svg" alt="logo"
@@ -16,7 +16,7 @@
         Menu
         <i class="fas fa-bars ms-1"></i>
       </button>
-      <div class="collapse navbar-collapse" id="navbarResponsive">
+      <div id="navbarResponsive" class="collapse navbar-collapse">
         <ul class="navbar-nav text-uppercase ms-auto py-4 py-lg-0">
           <li
             v-for="(navItem, navItemIndex) of headerList"
@@ -68,5 +68,79 @@ export default {
 .logo {
   color: white;
   font-size: 32px;
+}
+#mainNav {
+  padding-top: 1rem;
+  padding-bottom: 1rem;
+  background-color: #212529;
+}
+#mainNav .navbar-toggler {
+  padding: 0.75rem;
+  font-size: 0.75rem;
+  font-family: 'Montserrat', -apple-system, BlinkMacSystemFont, 'Segoe UI',
+    Roboto, 'Helvetica Neue', Arial, sans-serif, 'Apple Color Emoji',
+    'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji';
+  text-transform: uppercase;
+  font-weight: 700;
+}
+#mainNav .navbar-brand {
+  color: #ffc800;
+  font-family: 'Montserrat', -apple-system, BlinkMacSystemFont, 'Segoe UI',
+    Roboto, 'Helvetica Neue', Arial, sans-serif, 'Apple Color Emoji',
+    'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji';
+  font-weight: 700;
+  letter-spacing: 0.0625em;
+  text-transform: uppercase;
+}
+#mainNav .navbar-brand img {
+  height: 1.5rem;
+}
+#mainNav .navbar-nav .nav-item .nav-link {
+  font-family: 'Montserrat', -apple-system, BlinkMacSystemFont, 'Segoe UI',
+    Roboto, 'Helvetica Neue', Arial, sans-serif, 'Apple Color Emoji',
+    'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji';
+  font-size: 0.95rem;
+  color: #fff;
+  letter-spacing: 0.0625em;
+}
+#mainNav .navbar-nav .nav-item .nav-link.active,
+#mainNav .navbar-nav .nav-item .nav-link:hover {
+  color: #ffc800;
+}
+
+@media (min-width: 992px) {
+  #mainNav {
+    padding-top: 1.5rem;
+    padding-bottom: 1.5rem;
+    border: none;
+    background-color: #212529;
+    transition: padding-top 0.3s ease-in-out, padding-bottom 0.3s ease-in-out;
+  }
+  #mainNav .navbar-brand {
+    font-size: 1.5em;
+    transition: font-size 0.3s ease-in-out;
+  }
+  #mainNav .navbar-brand img {
+    height: 2rem;
+    transition: height 0.3s ease-in-out;
+  }
+  #mainNav.navbar-shrink {
+    padding-top: 1rem;
+    padding-bottom: 1rem;
+    background-color: #212529;
+  }
+  #mainNav.navbar-shrink .navbar-brand {
+    font-size: 1.25em;
+  }
+  #mainNav.navbar-shrink .navbar-brand svg,
+  #mainNav.navbar-shrink .navbar-brand img {
+    height: 1.5rem;
+  }
+  #mainNav .navbar-nav .nav-item {
+    margin-right: 1rem;
+  }
+  #mainNav .navbar-nav .nav-item:last-child {
+    margin-right: 0;
+  }
 }
 </style>
