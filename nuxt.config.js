c16ed4506b0fda58f936d9cb1adcd4c91ebb5b66
Marco V <48255395+zenimarc@users.noreply.github.com>
2022-05-22 19:13:21 +0200

    production ssr no static for heroku

--- nuxt.config.js
+++ nuxt.config.js
@@ -77,7 +77,10 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: 'http://localhost:3000',
+    baseURL:
+      process.env.NODE_ENV === 'production'
+        ? 'https://hyp-nuxt-proj.herokuapp.com/'
+        : 'http://localhost:3000',
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
