1bf1872c8af8cce9df1ad7447c6133b5fe2ad561
Marco V <48255395+zenimarc@users.noreply.github.com>
2022-05-22 19:32:42 +0200

    defer scripts

--- nuxt.config.js
+++ nuxt.config.js
@@ -45,7 +45,6 @@ export default {
       {
         // <!-- Bootstrap core JS-->
         src: 'https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js',
-        defer: true,
       },
       {
         // <!-- Font Awesome icons (free version)-->
