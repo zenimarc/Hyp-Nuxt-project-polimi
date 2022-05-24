1c7b2cfb8ccfb76240f5e6b5f577321a5388c23e
TeoSagli <79706196+TeoSagli@users.noreply.github.com>
2022-05-24 15:31:10 +0200

    fix styles

--- components/Service.vue
+++ components/Service.vue
@@ -1,7 +1,7 @@
 import VueTypeText from '@/vue-type-text.vue';
 <template>
   <div class="col-md-4">
-    <span class="fa-stack fa-4x">
+    <span class="fa-stack fa-8x">
       <img :src="img" />
     </span>
     <h4 class="my-3">{{ name }}</h4>
@@ -174,7 +174,6 @@ img {
   object-fit: cover;
 }
 tr {
-  font-size: 0.8vw;
   border: 1px solid currentColor;
   border-width: 1px;
   border-style: solid;
@@ -186,7 +185,7 @@ tbody > tr {
   border-bottom: 1px solid currentColor;
   border-width: 1px;
   border-style: solid;
-  border-color: rgba(0, 0, 0, 0.05);
+  border-color: #ced4da;
 }
 td,
 th {
@@ -197,7 +196,7 @@ tbody > tr > th {
   border-right: 1px solid currentColor;
   border-width: 1px;
   border-style: solid;
-  border-color: rgba(0, 0, 0, 0.05);
+  border-color: #ced4da;
 }
 div {
   height: 100%;
