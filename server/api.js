c0dd9302712dbe276481583b6dc933165a0428a2
TeoSagli <79706196+TeoSagli@users.noreply.github.com>
2022-05-30 23:39:52 +0200

    added cors to fix console error

--- server/api.js
+++ server/api.js
@@ -1,7 +1,9 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 const { Sequelize, DataTypes } = require('sequelize')
 const initialize = require('./initialize').default
+app.use(cors())
 app.use(express.json())
 
 let database = null
